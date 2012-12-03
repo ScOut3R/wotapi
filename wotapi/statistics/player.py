aa877df1174b3a4c3c237a0b8d26cb8e9e637c14
GABRI Mate <gabrimate@duosol.hu>
2012-12-03 16:40:58 +0100

    player statistics are self objects not a list

--- wotapi/statistics/player.py
+++ wotapi/statistics/player.py
@@ -23,15 +23,42 @@ class Player(object):
         f = request.read()
         request.close()
         soup = BeautifulSoup(f)
-        stats = []
 
         for lookup_string in sorted(self.lookup_strings_basic):
             temp = soup.find("td", text=lookup_string)
-            stats.append([ lookup_string[1:-2], temp.find_next_sibling().string ] )
+            if lookup_string == ' Defeats: ':
+                self.defeats = temp.find_next_sibling().string
+            if lookup_string == ' Battles Survived: ':
+                self.battles_survived = temp.find_next_sibling().string
+            if lookup_string == ' Hit Ratio: ':
+                self.hit_ratio = temp.find_next_sibling().string
+            if lookup_string == ' Maximum Experience per Battle: ':
+                self.maximum_xp_per_battle = temp.find_next_sibling().string
 
         for lookup_string in sorted(self.lookup_strings_rating):
             temp = soup.find("td", text=lookup_string)
-            stats.append([ lookup_string, temp.find_next_sibling().string, temp.find_next_sibling().find_next_sibling().string ] )
+            if lookup_string == 'Global Rating':
+                self.global_rating = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Victories/Battles':
+                self.victories_battles = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Average Experience per Battle':
+                self.average_xp_per_battle = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Victories':
+                self.victories = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string  == 'Battles Participated':
+                self.battles = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Capture Points':
+                self.capture_points = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Damage Caused':
+                self.damage_caused = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Defense Points':
+                self.defense_points = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Targets Destroyed':
+                self.targets_destroyed = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Targets Detected':
+                self.targets_detected = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
+            if lookup_string == 'Total Experience':
+                self.total_xp = { 'value': temp.find_next_sibling().string, 'place': temp.find_next_sibling().find_next_sibling().string }
     
     def single_stat(self, stat_name):
         request = urllib2.urlopen('http://worldoftanks%s/community/accounts/%s-%s/' % ( self.region, self.account_id, self.name))
